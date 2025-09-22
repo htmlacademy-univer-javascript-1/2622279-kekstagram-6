3dca9397f042931ae694aca59f69736893cac0f6
Полина Подвигина <podviginapolina@mail.ru>
2025-09-22 11:09:18 +0500

    Исправление

--- js/functions.js
+++ js/functions.js
@@ -1,25 +1,23 @@
-let checkStringLength = (str,maxLength) =>{
-  return str.length <= maxLength;
-}
-console.log(checkStringLength('проверяемая строка', 20));//true
-console.log(checkStringLength('проверяемая строка', 18)); // true
-console.log(checkStringLength('проверяемая строка', 10)); // false
+const checkStringLength = (string, length) => string.length <= length;
+checkStringLength('проверяемая строка', 20);//true
+checkStringLength('проверяемая строка', 18); // true
+checkStringLength('проверяемая строка', 10); // false
 
-let isPalindrome = (str) =>{
-  let cleanStr = str.replaceAll(' ', '').toLowerCase();
+const isPalindrome = (str) =>{
+  const cleanStr = str.replaceAll(' ', '').toLowerCase();
   const length = cleanStr.length;
   let newStr = '';
   for (let i = length-1; i>=0 ; i--){
     newStr += cleanStr[i];
   }
   return newStr === cleanStr;
-}
-console.log(isPalindrome('топот'));//true
-console.log(isPalindrome('ДовОд')); // true
-console.log(isPalindrome('Кекс'));  // false
-console.log(isPalindrome('Лёша на полке клопа нашёл ')); // true
+};
+isPalindrome('топот');//true
+isPalindrome('ДовОд'); // true
+isPalindrome('Кекс');  // false
+isPalindrome('Лёша на полке клопа нашёл '); // true
 
-let extractDigits = (str) =>{
+const extractDigits = (str) =>{
   if (typeof str === 'number') {
     str = str.toString();
   }
@@ -36,10 +34,10 @@ let extractDigits = (str) =>{
     return parseInt(digits, 10);
   }
   return NaN;
-}
-console.log(extractDigits("abc123def"));    // 123
-console.log(extractDigits("a1b2c3"));       // 123
-console.log(extractDigits("hello"));        // NaN
-console.log(extractDigits(""));             // NaN
-console.log(extractDigits('1 кефир, 0.5 батона')); // 105
-console.log(extractDigits(2023)); //2023
+};
+extractDigits('abc123def');    // 123
+extractDigits('a1b2c3');       // 123
+extractDigits('hello');        // NaN
+extractDigits('');             // NaN
+extractDigits('1 кефир, 0.5 батона'); // 105
+extractDigits(2023); //2023
