a5f5ad11b9fe78b34225afc83471ad6d1e34caf5
Полина Подвигина <podviginapolina@mail.ru>
2025-10-07 19:30:17 +0500

    Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -1,43 +1,14 @@
-const checkStringLength = (string, length) => string.length <= length;
-checkStringLength('проверяемая строка', 20);//true
-checkStringLength('проверяемая строка', 18); // true
-checkStringLength('проверяемая строка', 10); // false
-
-const isPalindrome = (str) =>{
-  const cleanStr = str.replaceAll(' ', '').toLowerCase();
-  const length = cleanStr.length;
-  let newStr = '';
-  for (let i = length-1; i>=0 ; i--){
-    newStr += cleanStr[i];
-  }
-  return newStr === cleanStr;
+const meetingVerification = (startWork, endWork, startMeeting, durationMeeting)=>{
+  const timeToMinutes = (timeStr) =>{
+    const [hours, minutes] = timeStr.split(':').map(Number);
+    return hours*60 + minutes;
+  };
+  return timeToMinutes(startMeeting) >= timeToMinutes(startWork) &&
+  timeToMinutes(startMeeting) + durationMeeting <=timeToMinutes(endWork);
 };
-isPalindrome('топот');//true
-isPalindrome('ДовОд'); // true
-isPalindrome('Кекс');  // false
-isPalindrome('Лёша на полке клопа нашёл '); // true
 
-const extractDigits = (str) =>{
-  if (typeof str === 'number') {
-    str = str.toString();
-  }
-
-  let digits = '';
-  for (let i = 0; i < str.length; i++) {
-    const char = str[i];
-    const num = parseInt(char, 10);
-    if (!Number.isNaN(num) && char >= '0' && char <= '9') {
-      digits += char;
-    }
-  }
-  if (digits.length > 0) {
-    return parseInt(digits, 10);
-  }
-  return NaN;
-};
-extractDigits('abc123def');    // 123
-extractDigits('a1b2c3');       // 123
-extractDigits('hello');        // NaN
-extractDigits('');             // NaN
-extractDigits('1 кефир, 0.5 батона'); // 105
-extractDigits(2023); //2023
+meetingVerification('08:00', '17:30', '14:00', 90); // true
+meetingVerification('8:0', '10:0', '8:0', 120);     // true
+meetingVerification('08:00', '14:30', '14:00', 90); // false
+meetingVerification('14:00', '17:30', '08:0', 90);  // false
+meetingVerification('8:00', '17:30', '08:00', 900); // false
